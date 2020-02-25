6c4c21a5622e329c92bdbe755f7fe8e58cfb4550
Jerem <jer.sou@gmail.com>
2020-02-25 18:13:38 +0100

    add hr repetition: update test.sh

--- test.sh
+++ test.sh
@@ -6,26 +6,30 @@ set -o pipefail
 
 echo ↓ ./example.sh --help
 ./example.sh --help
-echo 
+echo
 
 echo ↓ ./example.sh
 ./example.sh
-echo 
+echo
 
 echo ↓ ./example.sh main arg1 arg2
 ./example.sh main arg1 arg2
-echo 
+echo
 
 echo ↓ ./example.sh test arg1 arg2
 ./example.sh test arg1 arg2
-echo 
+echo
 
 NO_COLOR=true ./bash-utils.sh --help |
   grep utils: |
-  grep -vE "utils:help|utils:list_functions|utils:pipe_|utils:print_color|print_line|utils:run" |
+  grep -vE "utils:help|utils:list_functions|utils:pipe_|utils:print_color|print_line|utils:run|hr" |
   cut -d' ' -f2 |
   while read l; do
     echo "↓ ./bash-utils.sh $l msg"
     ./bash-utils.sh $l msg
     echo
   done
+
+echo ↓ TRACE=1 ./example.sh
+TRACE=1 ./example.sh
+echo
